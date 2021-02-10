08647e335ac103c79686685963773cf5ef812d70
Sunghwan-DS <jeonsung02@gmail.com>
2021-02-10 10:42:26 +0900

    init

---
+++ src/main/java/com/example/sunghwan/SunghwanApplication.java
@@ -0,0 +1,13 @@
+package com.example.sunghwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunghwanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunghwanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sunghwan/SunghwanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sunghwan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunghwanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
